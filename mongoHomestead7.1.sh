24b8c072107b5f4e5f44486966f2056e549a665d
Michael Musso <lynx@deltaaskii.com>
2017-04-24 15:36:29 +0200

    Fix creation mongodb.ini and link in right dirs
    Hi, this update fix a file creation and link in a cli e fpm directories.

--- mongoHomestead7.1.sh
+++ mongoHomestead7.1.sh
@@ -26,8 +26,9 @@ sudo pecl install mongodb;
 
 echo "adding the extension to your php.ini file";
 sudo touch /etc/php/7.1/mods-available/mongodb.ini
-sudo echo "; configuration for php mongo module\n; priority=30\nextension=mongodb.so" >> /etc/php/7.1/mods-available/mongodb.ini
-sudo ln -s /etc/php/7.1/mods-available/mongodb.ini 30-mongodb.ini
+sudo echo $'; configuration for php mongo module\n; priority=30\nextension=mongodb.so' >> /etc/php/7.1/mods-available/mongodb.ini
+sudo ln -s /etc/php/7.1/mods-available/mongodb.ini /etc/php/7.1/cli/conf.d/30-mongodb.ini
+sudo ln -s /etc/php/7.1/mods-available/mongodb.ini /etc/php/7.1/fpm/conf.d/30-mongodb.ini
 
 echo "Add mongodb.service file"
 cat >/etc/systemd/system/mongodb.service <<EOL
